72d31ba6622a0047d5a9192903a7e893b3b671f1
Alyssa Newhart <67665773+alyssanewhart@users.noreply.github.com>
2022-02-09 23:33:22 -0700

    card

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import {Card } from "@shopify/polaris";
 
 function index() {
     return (
+        <Card>
         <div>
             <p>Hello World</p>
         </div>
+        </Card>
     )
 }
 
