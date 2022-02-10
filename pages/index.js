01c3928e8c31ce792179a7e87d788d08c48aae16
Alyssa Newhart <67665773+alyssanewhart@users.noreply.github.com>
2022-02-10 16:57:41 -0700

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,48 @@
+import { AppProvider, Card, Page, ResourceList, TextStyle, Avatar} from "@shopify/polaris";
 import React from "react";
-import {Card } from "@shopify/polaris";
 
 function index() {
     return (
-        <Card>
+        <AppProvider>
+            <Page>
+            <TextStyle variation="strong">Hello World</TextStyle>
+    <Card>
+      <ResourceList
+        showHeader
+        items={[
+          {
+            id: 341,
+            url: 'customers/341',
+            name: 'Mae Jemison',
+            location: 'Decatur, USA',
+          },
+          {
+            id: 256,
+            url: 'customers/256',
+            name: 'Ellen Ochoa',
+            location: 'Los Angeles, USA',
+          },
+        ]}
+        renderItem={(item) => {
+          const {id, url, name, location} = item;
+          const media = <Avatar customer size="medium" name={name} />;
+
+          return (
+            <ResourceList.Item id={id} url={url} media={media}>
+              <h3>
+                <TextStyle variation="strong">{name}</TextStyle>
+              </h3>
+              <div>{location}</div>
+            </ResourceList.Item>
+          );
+        }}
+      />
+    </Card>
+  </Page>
         <div>
             <p>Hello World</p>
-        </div>
-        </Card>
+        </div>  
+        </AppProvider>
     )
 }
 
