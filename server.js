1d85bea733bfb131ee6bebe217d7b5cbf061f5a3
Alyssa Newhart <67665773+alyssanewhart@users.noreply.github.com>
2022-02-09 22:04:43 -0700

    server.js update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
-  SCOPES: process.env.SHOPIFY_API_SCOPES.split(","),
+  SCOPES: process.env.SCOPES.split(","),
   HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
   API_VERSION: ApiVersion.October20,
   IS_EMBEDDED_APP: true,
