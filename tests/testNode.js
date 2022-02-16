9a752b56a736c9ead2064abc06aff5d7a711bc2b
William Cook <61704930+WorkshopWeekly@users.noreply.github.com>
2022-02-16 14:59:12 -0700

    Update testNode.js

--- tests/testNode.js
+++ tests/testNode.js
@@ -1,3 +1,10 @@
 var express = require('express');
 var path = require('path');
+var logBackup = console.log;
+var logMessages = [];
+
+console.log = function() {
+    logMessages.push.apply(logMessages, arguments);
+    logBackup.apply(console, arguments);
+};
 express.static(path.join(__dirname, console.log(process.cwd())));
