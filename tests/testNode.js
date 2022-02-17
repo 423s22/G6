74a1cef2d1d3c1e72280b3d6d9ba297c1ead8eb2
William Cook <61704930+WorkshopWeekly@users.noreply.github.com>
2022-02-17 11:24:57 -0700

    Update testNode.js
    redesign of testNode.js

--- tests/testNode.js
+++ tests/testNode.js
@@ -1,10 +1,1 @@
-var express = require('express');
-var path = require('path');
-var logBackup = console.log;
-var logMessages = [];
-
-console.log = function() {
-    logMessages.push.apply(logMessages, arguments);
-    logBackup.apply(console, arguments);
-};
-express.static(path.join(__dirname, console.log(process.cwd())));
+var temp = require('../server');
