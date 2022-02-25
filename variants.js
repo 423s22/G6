0f67a49d3ca819831d6880f2b0aa2f0e516f6a29
William Cook <61704930+WorkshopWeekly@users.noreply.github.com>
2022-02-25 10:46:32 -0700

    Create variants.js
    back end variant JS code

---
+++ variants.js
@@ -0,0 +1,22 @@
+_onSelectChange: function() {
+      var variant = this._getVariantFromOptions();
+
+      this.$container.trigger({
+        type: 'variantChange',
+        variant: variant
+      });
+
+      if (!variant) {
+        return;
+      }
+
+      this._updateMasterSelect(variant);
+      this._updateImages(variant);
+      this._updatePrice(variant);
+      this._updateSKU(variant);
+      this.currentVariant = variant;
+
+      if (this.enableHistoryState) {
+        this._updateHistoryState(variant);
+      }
+    },
