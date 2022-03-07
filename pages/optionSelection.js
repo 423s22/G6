5ca147f4f63a2d0f258d6fb30ffaa229114644b9
Alyssa Newhart <67665773+alyssanewhart@users.noreply.github.com>
2022-03-06 22:32:13 -0700

    added product card

--- pages/optionSelection.js
+++ pages/optionSelection.js
@@ -3,13 +3,25 @@ import { Heading, Page, TextStyle, Layout, EmptyState, Form, FormLayout, TextFie
 import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
 import ProductList from '../components/ProductList';
 import Router from 'next/router';
-
+import ProductCard from '../components/ProductCard';
 
 class CustomOptions extends React.Component {
 
   state = { open: false };      // for resource picker
+  state = {productSelect: false};  // for display of resource picker
+  state = {product: null};
+
+  handleSelection = (resources) => {
+    this.setState({ open: false });
+    console.log(resources);
+    console.log(resources.selection[0].title)
+    console.log(resources.selection[0].images[0])
+    this.setState({product: resources})
+}
 
   render() {
+
+    if (!this.state.productSelect) {
     return (
 
       <Page>
@@ -20,10 +32,9 @@ class CustomOptions extends React.Component {
           showVariants={false}
           open={this.state.open}
           onSelection={(resources) => this.handleSelection(resources)}
-          onCancel={() => this.setState({ open: false })}
+          onCancel={() => {this.setState({ open: false }), this.setState({productSelect: true})}}
+          selectMultiple={false}
         />
-        
-
         <Layout>
           <EmptyState
             heading="Add product options"
@@ -38,11 +49,14 @@ class CustomOptions extends React.Component {
         </Layout> 
       </Page>
     );
+ 
   }
-  handleSelection = (resources) => {
-    this.setState({ open: false });
-    console.log(resources);
-}
+  else {
+    return (
+    <ProductCard product = {this.state.product} />
+    )
+  }
+  }
 }
 
 export default CustomOptions;
