9af829d4491530b8fc052fbe8d842ccf026f9140
Alyssa Newhart <67665773+alyssanewhart@users.noreply.github.com>
2022-03-27 17:37:39 -0600

    removed unecessary import statements

--- pages/add-options.js
+++ pages/add-options.js
@@ -1,11 +1,4 @@
 import React from 'react';
-import {Page, Layout, EmptyState, Banner} from "@shopify/polaris";
-import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
-import Router from 'next/router';
-import ProductCard from '../components/ProductCard';
-import AddOptions from '../components/AddOptions';
-import styles from '../css/add-options.module.css';
-import { useProductContext } from '../context/ProductContext';
 import SelectOptions from '../components/SelectOptions';
 
 class CustomOptions extends React.Component {
