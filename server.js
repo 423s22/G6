b02f20d8b834eecce073b4325a49e6340f5668aa
William Cook <61704930+WorkshopWeekly@users.noreply.github.com>
2022-04-01 17:08:07 -0600

    Database integration Beta V2

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const {verifyRequest} = require('@shopify/koa-shopify-auth');
 const {default: Shopify, ApiVersion} = require('@shopify/shopify-api');
 const Router = require('koa-router');
 const koaBody = require('koa-body');
+const db = require('./DB/handlerDB');
+const mysql = require('mysql-await');
 
 dotenv.config();
 const port = parseInt(process.env.PORT, 10) || 3000;
@@ -91,6 +93,24 @@ app.prepare().then(async () => {
 
   router.get("(/_next/static/.*)", handleRequest); // Static content is clear
   router.get("/_next/webpack-hmr", handleRequest); // Webpack content is clear
+  
+  db.connect("localhost",process.env.MYSQL_USER, process.env.MYSQL_KEY,"user");
+
+
+  router.get("/database/", async(ctx) => {
+    // Handle get request from database
+    await db.handleGetRequest(ctx);
+  });
+
+  router.post("/database/", async(ctx) => {
+    await db.handlePostRequest(ctx);
+  });
+
+  router.delete("/database/", async(ctx) => {
+    await db.handleDeleteRequest(ctx);
+  });
+  
+  
   router.get("(.*)", async (ctx) => {
     const shop = ctx.query.shop;
 
