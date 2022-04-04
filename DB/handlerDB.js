7087f2b9ab04660e1ddad62e5895749b459105c0
William Cook <61704930+WorkshopWeekly@users.noreply.github.com>
2022-04-04 10:12:05 -0600

    Reformat for the now Known JSON data structure

--- DB/handlerDB.js
+++ DB/handlerDB.js
@@ -1,11 +1,5 @@
 const { useQuery } = require('@apollo/client');
 const mysql = require('mysql-await');
-//import _create from "./crud";
-var pN = '"Dog Collar' + 'OwnerId"';
-const json = '{"productId": '+1+
-' , "productName": "Dog Collar" , "tableName": ' + pN +
-' "columnNames": ["productId", "productName", "baseCost", "apples", "bananas", "lemons"], "values": ['+
-1+', "Dog Collar", 1, ' + Math.random().toFixed(4) + ' ,'+ Math.random().toFixed(4)+', '+Math.random().toFixed(4)+']}';
 
 var isConnected = false;
 var con = null;
@@ -15,9 +9,9 @@ async function _checkConnect(host, username, password, database) {
     con = mysql.createPool({
         host: host,
         user: username,
-        password: password
+        password: password,
+        database: database
     });
-    await con.awaitQuery("CREATE DATABASE IF NOT EXISTS " + database + ";");
     
 }
 async function connect(host, username, password, database) {
@@ -35,6 +29,9 @@ async function disconnect(host, username, password, database) {
 }
 
 
+
+
+//Get Requests
 async function handleGetRequest(ctx) {
     ctx.respond = false;
     const requestedData = ctx.query.request;
@@ -51,15 +48,17 @@ async function handleGetRequest(ctx) {
 
 async function getUserProducts(data) {
     let results = await con.awaitQuery(
-        `SELECT * FROM  ` + data.tableName + ` WHERE userId = ` + data.userId + `;`
+        `SELECT * FROM  ` + data.optionType + ` WHERE productId = ` + data.userId + `;`
     );
     return JSON.stringify(results);
 }
 
+
+//Create Requests
 async function handlePostRequest(ctx) {
     const post = ctx.request.body;
     const data = JSON.parse(ctx.request.body);
-    await _createTable(JSON.parse(ctx.request.body));
+    await _createTable(data);
 
     const requestedOperation = post["operation"];
     let results;
@@ -78,21 +77,17 @@ async function handlePostRequest(ctx) {
 }
 
 async function _createProduct(data) {
+    //console.log(tempProductId);
     if(isNaN(data.productId)) {
-        let a = await _createHelp(data);
-        if(a) {
-            queryArr = _createBuilder(data.columnNames, data.values);
-            let result = await con.awaitQuery(
-                'INSERT INTO ' + data.tableName + '( '+ queryArr[0] + ' ) VALUES ( ' + queryArr[1] + ' );'
-            );
+        let a = await _createSearch(data);
+        if(!a) {
+            queryStr = _createHelp(data);
+            let result = await con.awaitQuery(queryStr);
             return JSON.stringify({ "insertId": result.insertId });
         }
         else {
-            const data = JSON.parse(json);
-            var q = _updateBuilder(data.columnNames, data.values, data.id);
-            var queryStatement ="UPDATE " + data.tableName + " SET ";
-            queryStatement += _updateBuilder(data.columnsTitles, data.values);
-            queryStatement += " WHERE ProductId = "+ data.id +";";
+            var queryStatement = _updateHelp(data);
+            
             let result = await con.awaitQuery(queryStatement) 
             return JSON.stringify({ "message": "Updated" });
         }
@@ -101,38 +96,125 @@ async function _createProduct(data) {
     }
 }
 
-async function _createHelp(data) {
+async function _createSearch(data) {
     if(isNaN(data.productId)) {
-        queryArr = _createBuilder(data.columnNames, data.values);
         let result = await con.awaitQuery(
-            'SElECT * FROM ' + data.tableName + ' WHERE productId = ' + data.productId + ';'
+            'SElECT * FROM ' + data.optionType + ' WHERE productId = ' + data.productId + ';'
         );
         return true; 
     } else {
         console.log("this is not being inserted" + data.productId);
+        return false;
     }
 }
 
-
-async function _createTable(json) {
-    const data = JSON.parse(json);
-    var queryStatement = "CREATE TABLE IF NOT EXISTS " + data.tableName + " (";
-    console.log("Create check");  
-    for(let i = 0; i < data.columnNames.length; i++) {
-        if( i == data.columnNames.length-1) {
-            queryStatement += data.columnNames[i] + " NUMERIC(10,4)";
-        }
-        else{
-            queryStatement += data.columnNames[i] + " NUMERIC(10,4), ";
-        }
+function _createHelp(data) {
+    //'INSERT INTO ' + data.optionType + '( '+ queryArr[0] + ' ) VALUES ( ' + queryArr[1] + ' );'
+    let queryTemp = 'INSERT INTO ' + data.optionType + '( ';
+    let queryStr = '';
+    if( data.optionType == "dropdown") {
+        queryStr = _createBuilderDrop(data);
     }
-    queryStatement += ");";
-    ProductId = data.ProductId;
-    var query_var = [ProductId];
-    return con.awaitQuery(queryStatement);
+    else {
+        queryStr = _createBuilderEngrave(data);
+    }
+    queryTemp += queryStr[0] + ' ) VALUES ( ' + queryStr[0] + ' );';
+    return queryTemp;
     
 }
 
+function _createBuilderDrop(data) {
+
+    let optionsLength = data.options.length;
+    
+    var query1 = "ProductId, ProductName, ";
+    var query2 = "" + data.productId + ", " + data.menuTitle + ", ";
+    for (let i = 0; i < optionsLength; i++) {
+        if( i == colLength-1) {
+            query1 += data.options[i]["label"];
+            query2 += data.options[i]["value"];
+        }
+        else{
+            query1 += data.options[i]["label"] + ", ";
+            query2 += data.options[i]["value"] + ", ";
+        }
+            
+    }
+    return [query1, query2]
+}
+
+function _createBuilderEngrave(data) {
+    var query1 = "ProductId, ProductName, Lines, Price";
+    var query2 = "" + data.productId + ", " + data.description + ", " + data.lines + ", " + data.price;
+    return [query1, query2]
+}
+
+async function _createTable(data) {
+    var queryStatement = "CREATE TABLE IF NOT EXISTS " + data.optionType + " (";
+    if(data.optionType == "dropdown") { 
+        queryStatement += _createTableHelp(data);
+    } else {
+        queryStatement += "productId NUMERIC(18,2), ProductName VARCHAR(100), Lines NUMERIC(10,0), Price NUMERIC(15,2);";
+    }
+    return con.awaitQuery(queryStatement);
+}
+
+function _createTableHelp(data) {
+    let queryStatement = "productId NUMERIC(18,2), productName VARCHAR(100), ";
+    for(let i = 0; i < data.options.length; i++) {
+        if( i == data.options.length-1) {
+            queryStatement += data.options[i]["label"] + " NUMERIC(10,4)";
+        }
+        else{
+            queryStatement += data.options[i]["label"] + " NUMERIC(10,4), ";
+        }
+    }
+    queryStatement += ");";
+    return queryStatement;
+}
+
+
+//Upate Requests
+function _updateHelp(data) {
+    let queryTemp ="UPDATE " + data.optionType + " SET ";
+
+    if( data.optionType == "dropdown") {
+        queryTemp += _updateBuilderDrop(data);
+    }
+    else {
+        queryTemp += _updateBuilderEngrave(data);
+    }
+    
+    queryTemp += " WHERE ProductId = "+ data.productId +";";
+    return queryTemp;
+}
+
+function _updateBuilderDrop(options) {
+
+    let optionsLength = options.length;
+    var queryTemp = "";
+    for (let i = 0; i < optionsLength; i++) {
+        if( i == colLength-1) {
+            queryTemp += options[i]["label"] + " = " + options[i]["value"];
+        }
+        else{
+            queryTemp += options[i]["label"] + " = " + options[i]["value"] + ", ";
+        }
+            
+    }
+    return queryTemp
+    
+}
+
+function _updateBuilderEngrave(data) {
+    var queryTemp = "ProductId = " + data.productId + ", ProductName = " + data.description + ", Lines = " + data.lines + ", Price = " +  data.price;
+    return queryTemp
+    
+}
+
+
+
+//Delete Requests
 async function handleDeleteRequest(ctx) {
     const requestedOperation = ctx.query.operation;
     let results;
@@ -158,68 +240,24 @@ async function handleDeleteRequest(ctx) {
 }
 
 async function _deleteProduct(data) {
-    if (isNaN(data.productId)) {
+    let tempNum = data.productId.replace("gid://shopify/Product/", '');
+    if (isNaN(tempNum)) {
         return JSON.stringify({ "message": "Invalid ID" });
     } else {
         let result = await con.awaitQuery(
-            "DELETE FROM  " + data.tableName + " WHERE ProductId = "+ data.productId +";"
+            "DELETE FROM  " + data.optionType + " WHERE ProductId = "+ data.productId +";"
         );
         return JSON.stringify({ "message": "Successfully deleted" });
-    }
-}
-async function _deleteTable(data) {
-    if (isNaN(data.tableName)) {
-        let result = await con.awaitQuery(
-            "DROP TABLE  " + data.tableName+";"
-        );
-        return JSON.stringify({ "message": "Successfully deleted" });
-    } else {
-        return JSON.stringify({ "message": "Table Not Deleted" });
     }
 }
 
-function _createBuilder(columnsTitles,values) {
-    let colLength = columnsTitles.length;
-    if( colLength == values.length ) {
-        var query1 = "";
-        var query2 = "";
-        for (let i = 0; i < colLength; i++) {
-            if( i == colLength-1) {
-                query1 += columnsTitles[i];
-                query2 += values[i];
-            }
-            else{
-                query1 += columnsTitles[i] + ", ";
-                query2 += values[i] + ", ";
-            }
-            
-        }
-        return [query1, query2]
-    }
-    else {
-        console.log("Columns and tableOptions are not equal");
-    }
-    
+async function _deleteTable(data) {
+    let result = await con.awaitQuery(
+        "DROP TABLE  " + data.optionType+";"
+    );
+    return JSON.stringify({ "message": "Successfully deleted" });
 }
-function _updateBuilder(columnsTitles,values) {
-    let colLength = columnsTitles.length;
-    if( colLength == values.length ) {
-        var query1 = "";
-        for (let i = 0; i < colLength; i++) {
-            if( i == colLength-1) {
-                query1 += columnsTitles[i] + " = " + values[i];
-            }
-            else{
-                query1 += columnsTitles[i] + " = " + values[i] + ", ";
-            }
-            
-        }
-        return query1
-    }
-    else {
-        console.log("Columns and tableOptions are not equal");
-    }
-}
+
 
 module.exports = {connect, disconnect, handleGetRequest, handleDeleteRequest, handlePostRequest, _checkConnect, _updateBuilder, _createBuilder, _createHelp,
 _deleteProduct, _deleteTable, isConnected, con};
