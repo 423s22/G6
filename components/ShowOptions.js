79bdc9fa37fe9d7c54edbe74c79a399e5460fb26
Alyssa Newhart <67665773+alyssanewhart@users.noreply.github.com>
2022-04-10 21:14:12 -0600

    Update ShowOptions return

--- components/ShowOptions.js
+++ components/ShowOptions.js
@@ -35,34 +35,51 @@ function ShowOptions() {
     }
   })
 }
-  if (optionsLoaded) {
-    console.log(productOptions)
-    productOptions.map((item) => (console.log(item.optionType)));
-    return(
-      <div className={styles.ShowOptionsCard}>
-        <Card>
-          <h2><b>Applied Options</b></h2>
-          {productOptions.map((item) => (
-              <div className={styles.optionDiv}>
-                  {item.optionType}
-              </div>
-          ))}
-      </Card>
-      </div>
-    );
-  }
 
-else {
-  return (
+if (optionsLoaded) {
+  console.log(productOptions)
+  console.log(productOptions.options)
+  productOptions.map((item) => (console.log(item.optionType)));
+  return(
     <div className={styles.ShowOptionsCard}>
       <Card>
-        <h2><b>Applied Options</b></h2>
-          <Button onClick={getOptions}>
-            Show Options 
-          </Button>
-      </Card>
+        <h2 className={styles.h2}><b>Applied Options</b></h2>
+        {productOptions.map((item, index) => (
+          <div>
+              {item.optionType == 'dropdown' ? (
+               <div className={styles.optionDiv} key={index}>
+                <p><b>Option type:</b> Dropdown</p>
+                <p>Menu title: {item.menuTitle} </p>
+                <p>Options: {item.options.map((option) => 
+                <span>{option}&nbsp;</span>
+                  )}</p>
+              {/* <span key={index}>Options: {item.options[0]} </span> */}
+               </div>
+              ) : (
+                <div className={styles.optionDiv} key={index}>
+                  <p><b>Option type:</b> Engraving</p>                
+                  <p>Description: {item.description} </p>
+                  <p>Number of lines: {item.lineNum} </p>
+                  <p>Price: {item.price} </p>
+                </div>
+              )}
+         </div>
+        ))}
+    </Card>
     </div>
   );
-  }
+}
+else {
+return (
+  <div className={styles.ShowOptionsCard}>
+    <Card>
+      <h2><b>Applied Options</b></h2>
+        <Button onClick={getOptions}>
+          Show Options 
+        </Button>
+    </Card>
+  </div>
+);
+}
 }
 export default ShowOptions;
