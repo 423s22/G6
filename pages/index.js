830eb892a7f71213ceca9c5446083c9bcce54367
Daniel Vinogradov <56127630+danel2233@users.noreply.github.com>
2022-04-18 17:57:28 -0600

    Homepage changes
    This is more or less me just quickly looking at how the structure is, I will ngrok when I am ready to fully commit.

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,20 @@ import Router from 'next/router';
 /* adding note here */
 class Index extends React.Component {
 
-  render() { 
-    return (
-      <AppProvider>
-        <Page>
-              {/* redirect user to custom options page */}  
-            {/* <Button onClick = {() => Router.push('/add-options')}>Product Selection Page</Button> */}      
-          <Layout>
-            <EmptyState
+  render(){
+  return (
+    <Page title="Home Page">
+      <Layout>
+        <Layout.Section oneHalf>
+          <Card title="Create Products">
+            <Card.Section>
+              <Thumbnail
+                source="https://thumbs.dreamstime.com/b/web-186774980.jpg"
+                size="large"
+                alt="Click"
+              />
+              
+              <EmptyState
               heading="Welcome to your custom options builder"
               action={{
                 content: "Products Options",
@@ -23,18 +29,39 @@ class Index extends React.Component {
               <h2>To start creating your options</h2>
             </EmptyState>
             
-              <h2>If you get lost along the way visit our FAQ page</h2>
-            <EmptyState
+            </Card.Section>
+
+           
+          </Card>
+        </Layout.Section>
+
+        <Layout.Section oneHalf>
+          <Card title="Samurai Shirt">
+            <Card.Section>
+              <Thumbnail
+                source="https://thumbs.dreamstime.com/b/faq-icon-vector-illustration-eps-146073139.jpg"
+                size="large"
+                alt="Samurai Shirt"
+              />
+
+<EmptyState
             action={{
               content: "FAQ Page",
               onAction: () => (Router.push('/FAQindex')),
             }}
           ></EmptyState>
-            </Layout>
-        </Page> 
-      </AppProvider>         
-    )
-  }
+            </Card.Section>
+            
+            <Card.Section>
+              <TextStyle varation="strong"> To view our most frequently asked questions press on the button above. </TextStyle>
+            </Card.Section>
+          </Card>
+        </Layout.Section>
+      </Layout>
+      ;
+    </Page>
+  );
+};
 }
 
 export default Index;
