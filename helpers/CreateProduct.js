c8524953c78011acfcf3c1f78088eb8cc4a66e1b
Alyssa Newhart <67665773+alyssanewhart@users.noreply.github.com>
2022-04-19 16:46:51 -0600

    create shopify product

---
+++ helpers/CreateProduct.js
@@ -0,0 +1,30 @@
+import authFetch from "./AuthFetch";
+
+// POST to backend route that create product in Shopify admin
+export default async function CreateProduct(data) {
+
+    // build product object
+    const product = {
+      "product": {
+          "title": `${data.title} ${data.description} ${data.optionType}`,
+          "product_type": `${data.optionType}`,
+          "variants": [
+            {
+              "title": `${data.title} ${data.description} ${data.optionType}`,
+              "price": `${data.price}`,
+          }
+        ],
+        "status": "draft",    // make it a draft product so it won't be shown on the store front
+    }
+  } 
+
+  const response = await authFetch("/api/add-engraving-product-option", {
+      method: "POST",
+     headers: {
+        Accept: "application/json"
+      },  
+      body: JSON.stringify(product),
+    }).then(res => res.text()).then(data => { return JSON.parse(data).product.id }).catch((error) => { console.log(error) })
+  
+    return response;
+    };
