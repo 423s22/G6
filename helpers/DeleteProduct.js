748555a8abf306cf4e3b5460e1458da8bcc5c4f8
Alyssa Newhart <67665773+alyssanewhart@users.noreply.github.com>
2022-04-20 18:04:14 -0600

    added product creation/deletion for dropdown options

--- helpers/DeleteProduct.js
+++ helpers/DeleteProduct.js
@@ -1,11 +1,11 @@
 import authFetch from "../utils/AuthFetch";
 
-// POST to backend route that create product in Shopify admin
+// DELETE to backend route that deletes product in Shopify admin
 export default async function DeleteProduct(productId) {
 
   const response = await authFetch(`/api/delete-product-option/${productId}`, {
       method: "DELETE",
-     headers: {
+      headers: {
         Accept: "application/json"
       },  
     }).then(res => res.text()).then(data => {}).catch((error) => { console.log(error) })
