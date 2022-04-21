0eb148922ac3092399c932dad782631f1b6886bd
Daniel Vinogradov <56127630+danel2233@users.noreply.github.com>
2022-04-21 16:10:25 -0600

    Checking fix for back button

--- pages/add-options.js
+++ pages/add-options.js
@@ -6,7 +6,7 @@ class CustomOptions extends React.Component {
   render() {
     return (
       <Page
-  breadcrumbs={[{content: 'Settings', url: '/settings'}]}
+  breadcrumbs={[{content: 'Homepage', url: '/index.js'}]}
   title="Add Product Options"
 >
   <Card title="Select Products to add." sectioned>
