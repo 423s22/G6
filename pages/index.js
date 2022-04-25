c4c8be6519eb98294bfe7221f49735551c4fe44f
Daniel Vinogradov <56127630+danel2233@users.noreply.github.com>
2022-04-25 00:33:14 -0600

    Changed some stuff
    seems there might be a bug somewhere, I'll revert back if I cant fix it rn

--- pages/index.js
+++ pages/index.js
@@ -9,68 +9,65 @@ class Index extends React.Component {
 
   render() {
     return (
-      <div>
-        <Page title="Rival Dynamic Pricing">
-          <Card title="About this app" sectioned>
-            <p>
-              This app focuses on dynamically changing the pricing for each item
-              purchased, Shopify does not have a dynamic price change for
-              variations of items. Total cost, including hidden variation costs,
-              is shown at the cart once you've finalized your purchases but
-              fails to show the pricing when you are shopping.
-            </p>
-          </Card>
-          <CalloutCard
-            title="Select and Create Products"
-            illustration="https://thumbs.dreamstime.com/b/web-186774980.jpg"
-            primaryAction={{
-              content: "Listed Products",
-              onAction: () => Router.push("/add-options")
-            }}
-          >
-            <p>
-              {" "}
-              The button below will lead you to the product management page,
-              where you may add products and their respective variations.
-            </p>
-          </CalloutCard>
+      <Page title="Rival Dynamic Pricing">
+        <Card title="About this app" sectioned>
+          <p>
+            This app focuses on dynamically changing the pricing for each item
+            purchased, Shopify does not have a dynamic price change for
+            variations of items. Total cost, including hidden variation costs,
+            is shown at the cart once you've finalized your purchases but fails
+            to show the pricing when you are shopping.
+          </p>
+        </Card>
+        <CalloutCard
+          title="Select and Create Products"
+          illustration="https://thumbs.dreamstime.com/b/web-186774980.jpg"
+          primaryAction={{
+            content: "Listed Products",
+            onAction: () => Router.push("/add-options")
+          }}
+        >
+          <p>
+            {" "}
+            The button below will lead you to the product management page, where
+            you may add products and their respective variations.
+          </p>
+        </CalloutCard>
 
-          <CalloutCard
-            title="Frequently Asked Questions"
-            illustration="https://thumbs.dreamstime.com/b/faq-icon-vector-illustration-eps-146073139.jpg"
-            primaryAction={{
-              content: "Listed Products",
-              onAction: () => Router.push("/FAQindex")
-            }}
-          >
-            <p>
-              If you have any questions on how to use the app please refer 
-              to the FAQ, there you will help on how to create, delete and modify
-              products.
-            </p>
-          </CalloutCard>
-        
-          <MediaCard
-  title="About Us"
-  description="Rival is a group of MSU students who are enthusiatic about
-  creating a user friendly dynamic pricing app for Shopify. The group is made up
-  of six individuals: Daniel Vinogradov (very handsome), Riley Slater,
+        <CalloutCard
+          title="Frequently Asked Questions"
+          illustration="https://thumbs.dreamstime.com/b/faq-icon-vector-illustration-eps-146073139.jpg"
+          primaryAction={{
+            content: "Listed Products",
+            onAction: () => Router.push("/FAQindex")
+          }}
+        >
+          <p>
+            If you have any questions on how to use the app please refer to the
+            FAQ, there you will help on how to create, delete and modify
+            products.
+          </p>
+        </CalloutCard>
+
+        <MediaCard
+          title="About Us"
+          description="Rival is a group of MSU students who are enthusiatic about creating a user friendly
+    dynamic pricing app for Shopify. The group is made up 
+    of six individuals: Daniel Vinogradov (very handsome), Riley Slater,
   James Marsh, Lenin Lewis, Alyssa Newhart, and Will Cook. The project was
  first conceptualized by Tessa who tried to help a client choose variation
  pricing for paintings of different sizes. From there, we have applied ourselves
  to create an app an that can cover all  product variations and their pricing."
->
-  <img
-    alt = "Rival Logo"
-    width="100%"
-    height="100%"
-    style={{objectFit: 'cover', objectPosition: 'center'}}
-    src="https://i.ibb.co/qjTQYGt/logo.png"
-  />
-</MediaCard>
-
-        </Page>
-      </div>
+        >
+          <img
+            alt="Rival Logo"
+            width="100%"
+            height="100%"
+            style={{ objectFit: "contain", objectPosition: "center" }}
+            src="https://i.ibb.co/qjTQYGt/logo.png"
+          /> 
+        </MediaCard>
+      </Page>
     );
   }
 }
