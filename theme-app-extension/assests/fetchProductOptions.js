917cbc27e1ede091ffa91b21e5ba4c4db80df499
Riley Slater <rileymail6@gmail.com>
2022-04-26 15:55:30 -0600

    uncomment

--- theme-app-extension/assests/fetchProductOptions.js
+++ theme-app-extension/assests/fetchProductOptions.js
@@ -2,8 +2,8 @@
 class fetchProductOptions {
     fetchOptions(productID) {
         let productData = fetch(`https://pricing-options-app.herokuapp.com/api/show-options/${productID}`)
-            .then(response => response.json());
-            //.then(data => console.log(data));
+            .then(response => response.json())
+            .then(data => console.log(data));
 
         return productData;
     }
