88f4ed71a183064837ebb886492b42cd0790d46a
Alyssa Newhart <67665773+alyssanewhart@users.noreply.github.com>
2022-05-03 11:12:11 -0600

    update variantIds

--- components/EngravingForm.js
+++ components/EngravingForm.js
@@ -67,7 +67,8 @@ function EngravingForm() {
 
      let info = await CreateProduct(engravingInfo);      // create product from option in Shopify and return back its productId
      engravingInfo.productOptionId = info.productOptionId;               // add ID of product option     
-     engravingInfo.variantOptionId = info.optionVariantId;               // add ID of product variant 
+     engravingInfo.optionVariantId = info.optionVariantId;               // add ID of product variant 
+     console.log(engravingInfo)
      updateDB(engravingInfo)                                        // call function to add option to DB
      }; 
 
